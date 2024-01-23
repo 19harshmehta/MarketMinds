4516cbb72971b8ee4aaccc9f53d5d382d4bf17ba
19harshmehta <mehtah631@gmail.com>
2024-01-23 14:23:59 +0530

    chart

--- src/main/java/com/arth/repository/EquityPriceDataRepository.java
+++ src/main/java/com/arth/repository/EquityPriceDataRepository.java
@@ -1,12 +1,19 @@
 package com.arth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.arth.entity.EquityPriceDataEntity;
+import java.util.List;
+import com.arth.entity.EquityEntity;
+
 
 @Repository
 public interface EquityPriceDataRepository extends JpaRepository<EquityPriceDataEntity,Integer>
 {
-
+	
+	
+	@Query(value = "select * from equity_price_data where equity_id = :equityId", nativeQuery = true)
+	List<EquityPriceDataEntity> findByEquityId(Integer equityId);
 }
